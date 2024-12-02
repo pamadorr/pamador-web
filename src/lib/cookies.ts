d0f6dae03d46478f5f7d52e5301c9b487008e286
seriovez <sahmedovicho@gmail.com>
2024-12-02 15:27:48 +0500

    upgraded from 14 to 15

--- src/lib/cookies.ts
+++ src/lib/cookies.ts
@@ -7,14 +7,14 @@ import { cookies } from 'next/headers'
  * @param {string} language - The language code to set.
  */
 export async function setLanguageCookie(language: string) {
-  const cookieStore = cookies()
+  const cookieStore = await cookies()
   cookieStore.set('languageId', language, {
     expires: new Date(Date.now() + 365 * 24 * 60 * 60 * 1000),
   })
   // await revalidatePath('/:path')
 }
 export async function getCurrentLanguage() {
-  const cookieStore = cookies()
+  const cookieStore = await cookies()
   const language = cookieStore.get('languageId')
 
   return language ? language.value : 'ru'
